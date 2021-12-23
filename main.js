ea605c7a1694ec121b59c3aa2b30cda4254dba03
zcnthn <63442977+zcnthn@users.noreply.github.com>
2021-12-23 14:40:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
             var formData = new FormData($("#profileUp")[0]);
 
             $.ajax({
-                url: "https://ighese.ir/logon.php",
+                url: "http://ighese.ir/Js/feedup.php",
                 type: 'POST',
                 data: formData,
                 contentType: false,
